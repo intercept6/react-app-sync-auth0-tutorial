35c1bf4fac58ab944a9a9fde8cc8a69804fff153
intercept6 <intercept6@gmail.com>
2020-05-08 19:44:03 +0900

    history分散を避けるための処理をApp.tsxに寄せる

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,29 @@ import { NavBar } from "./components/NavBar";
 import { Router, Route, Switch } from "react-router-dom";
 import { Profile } from "./components/Profile";
 import { DemoTable } from "./components/DemoTable";
-import { history } from "./utils/history";
+import { createBrowserHistory } from "history";
 import { PrivateRoute } from './components/PrivateRoute';
+import { AuthorizedApolloProvider } from './authorized-apollo-client';
+import { Auth0Provider } from './react-auth0-spa';
+import authConfig from './auth-config.json';
 
 export const App = () => {
+  const history = createBrowserHistory();
+
+  const onRedirectCallback = async (url?: string) => {
+    history.push(url ?? window.location.pathname);
+  };
+
   return (
     <div className="App">
+      <Auth0Provider
+        domain={authConfig.domain}
+        client_id={authConfig.clientId}
+        redirect_uri={window.location.origin}
+        audience={authConfig.audience}
+        onRedirectCallback={onRedirectCallback}
+      >
+        <AuthorizedApolloProvider>
       <Router history={history}>
         <header>
           <NavBar />
@@ -19,6 +36,9 @@ export const App = () => {
           <PrivateRoute path="/demo-table" component={DemoTable} />
         </Switch>
       </Router>
+
+        </AuthorizedApolloProvider>
+      </Auth0Provider>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,28 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
-import { history } from './utils/history';
-import { Auth0Provider } from './react-auth0-spa';
-import authConfig from './auth-config.json';
-import { AuthorizedApolloProvider } from './authorized-apollo-client';
-
-const onRedirectCallback = async (url?: string) => {
-  history.push(url ?? window.location.pathname);
-};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={authConfig.domain}
-      client_id={authConfig.clientId}
-      redirect_uri={window.location.origin}
-      audience={authConfig.audience}
-      onRedirectCallback={onRedirectCallback}
-    >
-      <AuthorizedApolloProvider>
-        <App />
-      </AuthorizedApolloProvider>
-    </Auth0Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
